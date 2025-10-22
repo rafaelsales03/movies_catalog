c5196e8259e44349d35c6312fbb4191eb53ddc75
rafaelsales03 <elrafael76234@gmail.com>
2025-10-22 15:40:50 -0300

    0008 [Chore]: Adjust code formatting to comply with RuboCop rules

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "movies#index"
-  
+
   devise_for :users
-  resources :movies do 
-    resources :comments, only: [:create]
+  resources :movies do
+    resources :comments, only: [ :create ]
   end
 end
