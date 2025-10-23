1d9a48c3db4ceb48b47de995bc6b74270bd95ef5
rafaelsales03 <elrafael76234@gmail.com>
2025-10-23 12:09:33 -0300

    0010 [Chore]: Deploy rails application on render cloud platform

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,19 +56,20 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-
-  # Set host to be used by links generated in mailer templates.
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("APP_DOMAIN", "localhost"),
+    protocol: "https"
+  }
 
-  config.action_mailer.default_url_options = { host: "SEU_DOMINIO_AQUI.com", protocol: "https" }
-
-  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  # Configuração SMTP do SendGrid
   config.action_mailer.smtp_settings = {
-    user_name: Rails.application.credentials.dig(:sendgrid, :user_name),
-    password: Rails.application.credentials.dig(:sendgrid, :password),
+    user_name: ENV.fetch("SENDGRID_USERNAME", "apikey"),
+    password: ENV.fetch("SENDGRID_PASSWORD", ""),
     address: "smtp.sendgrid.net",
-    port: 465,
+    port: 587,
     authentication: :plain,
     enable_starttls_auto: true
   }
